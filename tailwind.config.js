46101a46bc9c6a7991458aed5fd6dc418d597d57
Nguyễn Quốc Trọng Nghĩa <nghia.nguyen170192@gmail.com>
2024-10-22 22:36:40 +0700

    commit message

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import typographyPlugin from "@tailwindcss/typography";
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--aw-color-primary)",
+        secondary: "var(--aw-color-secondary)",
+        accent: "var(--aw-color-accent)",
+        default: "var(--aw-color-text-default)",
+        muted: "var(--aw-color-text-muted)",
+      },
+      fontFamily: {
+        sans: [
+          "var(--aw-font-sans, ui-sans-serif)",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        serif: [
+          "var(--aw-font-serif, ui-serif)",
+          ...defaultTheme.fontFamily.serif,
+        ],
+        heading: [
+          "var(--aw-font-heading, ui-sans-serif)",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: "", // don’t generate the pseudo-element
+              //                content: '""', // this is an alternative: generate pseudo element using an empty string
+            },
+            "code::after": {
+              content: "none",
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [typographyPlugin],
+  darkMode: "class",
+};
